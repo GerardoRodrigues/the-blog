df24d00964d8781973881572324ea154d68dcf81
Gerardo Rodrigues <gerardofilho56@gmail.com>
2026-02-09 14:39:58 -0300

    container component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
+import { Container } from "@/components/Container/Container";
 import { PostList } from "@/components/PostsList/PostsList";
 import { SpinLoader } from "@/components/SpinLoader/SpinLoader";
 import { Suspense } from "react";
 
 export default async function Home() {
   return (
-    <div>
-      <header className="text-6xl font-bold text-center">Header</header>
+    <Container>
+      <header className="text-6xl font-bold text-center py-8">Header</header>
       <Suspense fallback={<SpinLoader className="min-h-20" />}>
         <PostList />
       </Suspense>
-    </div>
+    </Container>
   );
 }
--- components/SpinLoader/SpinLoader.tsx
+++ components/SpinLoader/SpinLoader.tsx
@@ -1,12 +1,10 @@
-type SpinLoaderProps = {
+interface SpinLoaderProps {
   className?: string;
-};
+}
 
 export function SpinLoader({ className = "" }: SpinLoaderProps) {
   return (
-    <div
-      className={`flex items-center justify-center ${className}`}
-    >
+    <div className={`flex items-center justify-center ${className}`}>
       <div className="h-10 w-10 border-5 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
